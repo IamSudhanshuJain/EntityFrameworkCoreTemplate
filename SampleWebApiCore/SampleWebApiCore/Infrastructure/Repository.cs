475a6f374d32e36658ca34750e0426c81b20989d
Sudhanshu Jain <SJain@kaplaninc.com>
2022-03-19 16:38:31 +0530

    commit the changes for configuration

--- SampleWebApiCore/SampleWebApiCore/Infrastructure/Repository.cs
+++ SampleWebApiCore/SampleWebApiCore/Infrastructure/Repository.cs
@@ -17,14 +17,28 @@ namespace Infrastructure
             _entity = _context.Set<T>();
         }
 
-        public T Get(long id)
+        public T Get(object id)
         {
             return _entity.Find(id);
         }
+
+        public void Save(T data)
+        {
+            _context.Add(data);
+            _context.SaveChanges();
+        }
+        public void Update(T data)
+        {
+            _context.Entry(data).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
     }
+
     public interface IRepository<T> where T : class
-    {     
-        public T Get(long Id);
-        
+    {
+        public T Get(object Id);
+        public void Save(T Data);
+        public void Update(T Data);
+
     }
 }
---
+++ SampleWebApiCore/SampleWebApiCore/Service/DepartmentService.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure;
+using Infrastructure.DomainEntities;
+using System;
+
+namespace Service
+{
+    public class DepartmentService: IDepartmentService
+    {
+        private readonly IRepository<Department> _departmentRepository;
+        public DepartmentService(IRepository<Department> departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+
+        }
+
+        public Department GetDetails(int i)
+        {
+            return _departmentRepository.Get(i);
+        }
+
+        public void SaveInformation(Department department)
+        {
+            _departmentRepository.Save(department);
+        }
+        public void UpdateInformation(Department department, int Id)
+        {
+            var existing = _departmentRepository.Get(Id);
+            if (existing != null)
+            {
+                existing.Name = department.Name;
+                existing.Description = department.Description;  
+                _departmentRepository.Update(existing);
+            }
+        }
+    }
+}
